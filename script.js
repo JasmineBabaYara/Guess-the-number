ed24b86dd5cde130876434ea93bd1e858bad06fa
jasmine baba yara <JasmineBabaYara>
2021-03-11 18:30:02 +0000

    create html file

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 // Guess the number
 
+
+
+/*let answer = 5;
+let chancesleft = 3;
+let btn = document.getElementById('btn');
+let output = document.getElementById('outputtext');
+
+ btn.addEventListener('click', function() {
+
+    let guess = document.getElementById('guess').value;
+
+        if (guess == answer) {
+            output.innerHTML = 'You guessed correctly'
+        }else if(guess!= answer){ 
+            output.innerHTML = 'Try again'
+            }
+
+            while(chancesleft != 0) {
+                chancesleft -= 0;
+                if(chancesleft = 0) {
+                    document.getElementById('guess').remove
+                }
+
+            }
+        
+    });*/
+
+//Guess the number 2
+
 document.querySelector('#guessthenumber-button').addEventListener('click', guessthenumber);
 
-let answer = 5;
-let chancesleft = 3;
+    let answer = 5;
+    let chancesleft = 3;
 
-function guessthenumber() {
-    let guess = prompt('Guess the number');
+    function guessthenumber() {
+        let guess = prompt('Guess the number');
 
-    if(guess == answer) {
-        prompt('You guessed correctly');
-    }
     
-    while( chancesleft != 0) {
-        chancesleft -= 1
-
-    } if(guess != answer) {
-        prompt('Try again');
+           while(chancesleft != 0) {
+                chancesleft -= 1;
+           if(guess == answer){
+                prompt('You guessed correctly');
     
-    }
-}
+            }else{
+                prompt('Try again');
+            }
+
+            if (chancesleft == 0 ) {
+                prompt('You are out of chances');
+            }
+        }
+
+
+    } 
+
+
+
+    
