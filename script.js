5f0467e080163c04551843a38de2e7542cf451fd
jasmine baba yara <JasmineBabaYara>
2021-04-29 20:48:32 +0000

    updated code

--- script.js
+++ script.js
@@ -1,60 +1,24 @@
-// Guess the number
-
-
-
-/*let answer = 5;
-let chancesleft = 3;
+let answer = 5;
 let btn = document.getElementById('btn');
 let output = document.getElementById('outputtext');
+let chancesleft = 3;
 
- btn.addEventListener('click', function() {
-
+btn.addEventListener('click', function() {
     let guess = document.getElementById('guess').value;
-
-        if (guess == answer) {
-            output.innerHTML = 'You guessed correctly'
-        }else if(guess!= answer){ 
-            output.innerHTML = 'Try again'
-            }
-
-            while(chancesleft != 0) {
-                chancesleft -= 0;
-                if(chancesleft = 0) {
-                    document.getElementById('guess').remove
-                }
-
-            }
+    chancesleft -= 1;
+   while(chancesleft != 0 ){
         
-    });*/
-
-//Guess the number 2
-
-document.querySelector('#guessthenumber-button').addEventListener('click', guessthenumber);
-
-    let answer = 5;
-    let chancesleft = 3;
-
-    function guessthenumber() {
-        let guess = prompt('Guess the number');
-
-    
-           while(chancesleft != 0) {
-                chancesleft -= 1;
-           if(guess == answer){
-                prompt('You guessed correctly');
-    
-            }else{
-                prompt('Try again');
-            }
-
-            if (chancesleft == 0 ) {
-                prompt('You are out of chances');
-            }
+       console.log(chancesleft);
+   
+        if(guess == answer) {
+            output.innerHTML='You guessed correctly';
+        }else{
+            output.innerHTML = 'Try again!'
         }
+    }
 
-
-    } 
-
-
-
-    
+        if (chancesleft = 0) {
+            output.innerHTML ='You are out of chances';
+        
+    }
+    });
