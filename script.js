6f377ca891603c90b9cf6f43a50665a52dd5fc9c
JasmineBabaYara <jassyprincess12gmail.com>
2021-06-12 14:21:29 -0700

    create js file

--- script.js
+++ script.js
@@ -5,20 +5,18 @@ let chancesleft = 3;
 
 btn.addEventListener('click', function() {
     let guess = document.getElementById('guess').value;
-    chancesleft -= 1;
-   while(chancesleft != 0 ){
-        
-       console.log(chancesleft);
-   
-        if(guess == answer) {
-            output.innerHTML='You guessed correctly';
-        }else{
-            output.innerHTML = 'Try again!'
-        }
-    }
+        chancesleft -= 1;
 
-        if (chancesleft = 0) {
-            output.innerHTML ='You are out of chances';
+        if(chancesleft != 0){
+            if(guess == answer) {
+                output.innerHTML='You guessed correctly';
+            }else{
+                output.innerHTML = 'Try again!'
+            }
+        }
         
-    }
-    });
+        if (chancesleft == 0) {
+            output.innerHTML ='You are out of chances';
+            document.getElementById('guess').remove();
+        }
+});
